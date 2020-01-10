3a1117f4752a6040c7a10aa29be9f855902590ae
john-mani <59623585+john-mani@users.noreply.github.com>
2020-01-10 09:34:44 -0500

    Add files via upload

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
-	"log"
+	"io/ioutil"
 	"net/http"
 	"strings"
 	"sync"
@@ -24,67 +24,23 @@ type Order struct {
 	UpdatedAt string `xml:"updatedAt" json:"updatedAt"`
 }
 
-var testXML string = a
+//Get and process data
+func serveJSON(z http.ResponseWriter, req *http.Request) {
 
-// Test case data 1
-var a string = `
-<orderList>	
-	<order>
-		<id>aeffb38f-a1a0-48e7-b7a8-2621a2678534</id>
-		<data>first_Order_Data</data>
-		<createdAt>0001-01-01T00:00:00Z</createdAt>
-		<updatedAt>0001-01-01T00:00:00Z</updatedAt>
-	</order>
-	<order>
-		<id>beffb38f-b1a0-58e7-c7a8-3621a2678534</id>
-		<data>second_Order_Data</data>
-		<createdAt>0001-01-01T00:00:00Z</createdAt>
-		<updatedAt>0001-01-01T00:00:00Z</updatedAt>
-	</order>
-</orderList>
-`
+	//get data from request body
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	//log.Println(string(body))
 
-//Test case data 2
-var b string = `
-<orderList>	
-	<order>
-		<id>asfd678f-df88-dfd8-dfd7-vc78bc87xv89</id>
-		<data>order data 1</data>
-		<createdAt>0001-01-01T00:00:00Z</createdAt>
-		<updatedAt>0001-01-01T00:00:00Z</updatedAt>
-	</order>
-	<order>
-		<id>asdf234-sd7f8-df8-asd98-vzx87vzx65asd</id>
-		<data>order data 2</data>
-		<createdAt>0001-01-01T00:00:00Z</createdAt>
-		<updatedAt>0001-01-01T00:00:00Z</updatedAt>
-	</order>
-	<order>
-		<id>asdf234-sd7f8-df8-asd98-vzx87vzx65asd</id>
-		<data>order data 3</data>
-		<createdAt>0001-01-01T00:00:00Z</createdAt>
-		<updatedAt>0001-01-01T00:00:00Z</updatedAt>
-	</order>
-	<order>
-		<id>asdf234-sd7f8-df8-asd98-vzx87vzx65asd</id>
-		<data>order data 4</data>
-		<createdAt>0001-01-01T00:00:00Z</createdAt>
-		<updatedAt>0001-01-01T00:00:00Z</updatedAt>
-	</order>
-	
-	
-</orderList>
-`
-
-//create variable to hold XML data
-var data = &OrderList{}
-
-func main() {
+	//create variable to hold XML data
+	var data = &OrderList{}
 
 	//Unmarshal Data
-	err := xml.Unmarshal([]byte(testXML), data)
+	err = xml.Unmarshal(body, data)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
 	//Create Waitgroup
@@ -104,15 +60,15 @@ func main() {
 	} //End For Loop
 	wg.Wait() //Wait for goroutines to finish
 
+	//Marshal data and output as JSON
+	jsonData, _ := json.Marshal(data)
+	fmt.Fprint(z, string(jsonData))
+}
+
+func main() {
+
 	//serve JSON Data to server at port:8080
 	http.HandleFunc("/", serveJSON)
 	http.ListenAndServe(":8080", nil)
 
-}
-
-//JSON server function
-func serveJSON(z http.ResponseWriter, r *http.Request) {
-	//Marshal data and output as JSON
-	jsonData, _ := json.Marshal(data)
-	fmt.Fprint(z, string(jsonData))
 }
